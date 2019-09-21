3c44ba122c200a8e6d8c144b0f3da239591c0036
yangbaihua <baihuayang881111@163.com>
2019-09-21 21:28:47 +0800

    [lab2] add test script for reElection

---
+++ src/raft/my_test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+>result.txt
+rm -f logs/*
+if [ "$#" -ne 1 ]; then
+    echo "Usage: args number must be 1"
+    exit 1
+fi
+max=100
+succeedCount=0
+for i in `seq 1 $max`
+do
+   >tmp.txt
+   #go test -run $1 | grep 'ok' | grep 'raft' >> result.txt
+   #res=`go test -run $1 | grep 'ok' | grep 'raft'` 
+   #goRes=`go test -run $1`
+   go test -run $1>tmp.txt
+   
+   okRes=`grep 'ok' tmp.txt | grep 'raft'`
+   #echo $okRes 
+   if [ ! -z "$okRes" ]; then
+       succeedCount=$(($succeedCount+1))
+   else
+       errTxt=$1_error_$i.log
+       cp tmp.txt logs/$errTxt
+   fi
+   echo "Test $i done"
+done
+rm tmp.txt
+echo "total succeed $succeedCount in $max"
+exit 1
+
